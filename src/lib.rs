58725aef77d8409c78bf861dc1172fa7ffe23212
Randy Barlow <randy@electronsweatshop.com>
2023-10-16 18:09:11 -0400

    Convert the public interface to use the Read trait
    This should allow more flexibility for us in the future, allowing us to
    eventually be able to stream the results without having to hold all the
    data in memory.
    
    For now we continue to hold the data in memory as I have not adjusted
    the internal APIs.
    
    Signed-off-by: Randy Barlow <randy@electronsweatshop.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,39 @@
 //! beeziptoo
 //!
 //! Because we wanted to implement `bzip2`, too.
-use thiserror::Error as ThisError;
+use std::io::{self, Cursor, Read};
 
 mod rle;
 
+/// These are the possible errors that can occur during compression.
+#[derive(Debug, thiserror::Error)]
+pub enum CompressError {
+    /// An IO error occurred.
+    #[error("I/O error: {0}")]
+    IOError(io::Error),
+}
+
+impl From<io::Error> for CompressError {
+    fn from(value: io::Error) -> Self {
+        CompressError::IOError(value)
+    }
+}
+
 /// These are the possible errors that can occur during decompression.
-#[derive(Debug, ThisError)]
+#[derive(Debug, thiserror::Error)]
 pub enum DecompressError {
+    /// An IO error occurred.
+    #[error("I/O error: {0}")]
+    IOError(io::Error),
     /// The runlength decoder encountered an invalid input.
     #[error("Failed to decode at a runlength step")]
     RunLengthDecode,
+}
+
+impl From<io::Error> for DecompressError {
+    fn from(value: io::Error) -> Self {
+        DecompressError::IOError(value)
+    }
 }
 
 impl From<rle::Error> for DecompressError {
@@ -22,8 +45,18 @@ impl From<rle::Error> for DecompressError {
 }
 
 /// Compress the given data.
-pub fn compress(data: &[u8]) -> Vec<u8> {
-    rle::forward(data)
+pub fn compress<R>(mut data: R) -> Result<impl Read, CompressError>
+where
+    R: Read,
+{
+    let mut all_data = vec![];
+    data.read_to_end(&mut all_data)?;
+
+    let rle_data = rle::forward(&all_data);
+
+    let cursor = Cursor::new(rle_data);
+
+    Ok(cursor)
 }
 
 /// Decompress the given data.
@@ -31,6 +64,16 @@ pub fn compress(data: &[u8]) -> Vec<u8> {
 /// # Errors
 ///
 /// This function is failable since it is possible the given data isn't a valid `bzip2` archive.
-pub fn decompress(data: &[u8]) -> Result<Vec<u8>, DecompressError> {
-    rle::reverse(data).map_err(|e| e.into())
+pub fn decompress<R>(data: &mut R) -> Result<impl Read, DecompressError>
+where
+    R: Read,
+{
+    let mut all_data = vec![];
+    data.read_to_end(&mut all_data)?;
+
+    let un_rle_data = rle::reverse(&all_data)?;
+
+    let cursor = Cursor::new(un_rle_data);
+
+    Ok(cursor)
 }
